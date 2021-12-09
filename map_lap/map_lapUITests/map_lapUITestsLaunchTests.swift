ad3425df33aef34457e081231a6d0b0d919801fe
DAHMAJABER <dahma.alwani.jiz.s@tuwaiq.edu.sa>
2021-12-09 11:10:29 +0300

    map is done

---
+++ map_lap/map_lapUITests/map_lapUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  map_lapUITestsLaunchTests.swift
+//  map_lapUITests
+//
+//  Created by dahma alwani on 04/05/1443 AH.
+//
+
+import XCTest
+
+class map_lapUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
